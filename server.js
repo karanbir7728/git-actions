030bbd6e16f7d64372db3d4eba52ba90b4de4589
karanbir <karanbir@localhost.localdomain>
2025-04-04 11:09:22 -0600

    new

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Action s CI. SAIT ID:000123456');
+  res.send('CPSY 350 Project: Github Action s CI. SAIT ID:00935201');
 });
 
 const server = app.listen(PORT, HOST);
